08c59b2ba4bd3c90a58bb1b484272dbd8ca0d725
IthHydra <138935149+IthHydra@users.noreply.github.com>
2023-07-26 10:02:34 -0700

    index.ts
    Make big messages split

---
+++ src/plugins/1234discordmessage/index.ts
@@ -0,0 +1,16 @@
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "Split Message",
+    description: "make big messages possible",
+    authors: [
+        {
+            id: 792231107940581417n,
+            name: "ItsHydra0",
+        },
+    ],
+    patches: [],
+    // Delete these two below if you are only using code patches
+    start() {},
+    stop() {},
+});
